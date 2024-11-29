79c31b3ef0c8843bd6a5f3642e8f33e361c668b4
Beratdmrbs <beratdmrbs61@gmail.com>
2024-11-29 21:51:22 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+
+class Program
+{ static void Main()
+    {
+        ArrayList isimler = new ArrayList();
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine("Bir isim giriniz: ");
+            string isim = Console.ReadLine();
+            isimler.Add(isim);
+        }
+        isimler.Sort();
+        Console.WriteLine("\nAlfabetik sıralı isimler:");
+        foreach (string isim in isimler)
+        {
+            Console.WriteLine(isim);
+        }
+    }
+}
